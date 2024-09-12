efcc755f88eeb4cd13ca15136e9c72ab49684131
mloard <mloard2011@hotmail.com>
2024-09-11 22:47:42 -0400

    fixing E128 lint error

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,9 +15,8 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
 
-# from .restapis import get_request, analyze_review_sentiments, post_review
-from .restapis import get_request, analyze_review_sentiments
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -126,7 +125,8 @@ def add_review(request):
     if (request.user.is_anonymous is False):
         # data = json.loads(request.body)
         try:
-            # response = post_review(data)
+            response = post_review(data)
+            print(response)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({"status": 401, "message": "Error in posting review"})   # noqa: E501
